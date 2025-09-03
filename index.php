4e811a40324fe674d886a39b003c5fb8907c7484
Samy Mizi Allaoua <2393410@cmontmorency.qc.ca>
2025-09-03 14:19:22 -0400

    correction nom db

--- index.php
+++ index.php
@@ -2,16 +2,16 @@
 <html>
     <head>
         <meta charset="utf-8" />
-        <title>Concessionaire</title>
+        <title>Concessionnaire</title>
     </head>
 
     <body>
-        <h1>Concessionaire Auto</h1>
+        <h1>Concessionnaire Auto</h1>
 
         <?php
 
         try {
-            $bdd = new PDO('mysql:host=localhost;dbname=concessionaire;charset=utf8', 'root', 'mysql');
+            $bdd = new PDO('mysql:host=localhost;dbname=concessionnaire;charset=utf8', 'root', 'mysql');
         } catch (Exception $e) {
             die('Erreur : ' . $e->getMessage());
         }
