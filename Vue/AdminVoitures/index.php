2797fc7e9c3e4bde97a631e856c5aa68b538dd9c
SamyMZA <2393410@cmontmorency.qc.ca>
2025-09-20 16:06:46 -0400

    Fonctionnement de l'authentification

---
+++ Vue/AdminVoitures/index.php
@@ -0,0 +1,20 @@
+<?php $this->$titre = "Concessionnaire Auto"; ?>
+
+
+<div id="pageAccueil">
+    <div id = "annonces">
+        <?php foreach($voitures as $voiture): ?>
+            <div class="voiture" for> 
+                <img src= <?= $voiture['img'] ?> />
+                <a href="<?= "AdminVoitures/voiture/" . $voiture['id'] ?>"> 
+                    <h2> <?= $voiture['prix'] ?> $ </h2>
+                    <p> <?= $voiture['marque'] ?> </p>
+                    <p> <?= $voiture['modele'] ?> </p>             
+                </a> 
+            </div>
+        <?php endforeach; ?>    
+        
+        
+    </div>
+
+</div>
--- Vue/gabarit.php
+++ Vue/gabarit.php
@@ -6,13 +6,19 @@
         <base href="<?= $racineWeb ?>" >
         <link rel="stylesheet" href="Contenu/style.css" />
         <title><?= $titre ?></title>
-
     </head>
     <body>
         <div id="global">
             <header>
                 <h1 id="titreSite">Concessionnaire Auto</h1>
                 <p>Je vous souhaite la bienvenue sur notre concessionnaireAuto !.</p>
+                <?php if (isset($utilisateur)) : ?>
+                        <h3>Bonjour <?= $utilisateur['nom'] ?>,
+                            <a href="Utilisateurs/deconnecter"><small>[Se déconnecter]</small></a>
+                        </h3>
+                <?php else : ?>
+                        <h3>[<a href="Utilisateurs/index">Se connecter</a>] <small>(admin/admin)</small></h3>
+                <?php endif; ?>
             </header>
             <div id="contenu">
                 <?= $contenu ?>
