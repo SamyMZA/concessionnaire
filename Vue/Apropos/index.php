a1ceaf22324723736eb8c69b3990eae4b51f06a0
SamyMZA <2393410@cmontmorency.qc.ca>
2025-09-22 06:36:53 -0400

    [ADD] Apropos + Affichage deuxieme table

---
+++ Vue/Apropos/index.php
@@ -0,0 +1,23 @@
+<?php $titre = 'À propos Concessionnaire'; ?>
+
+<div id="apropos-page">
+    <h3>Concessionnaire Auto</h3>
+    <ul>
+        <p>Concessionnaire auto ou on peut achter une voiture, supprimer ou annuler notre achat. On peut (avec un compte) créer une annonce et modifer </p>
+    </ul>
+    
+
+    <h2>Base de donne</h2>
+
+    <a href=""></a>
+
+    <br>
+
+
+    <h2>Lien table</h2>
+
+    <p>un utilisateur peut acheter plusieurs voitures</p>
+    <p>une voiture peut et etre a cheter pas plusieurs utilisateur (offre)</p>
+    <p>table achat</p>
+
+</div>
