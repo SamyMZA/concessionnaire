60bbc0ba20bea3bb7cd6c5fafa02443a07ec46d5
SamyMZA <2393410@cmontmorency.qc.ca>
2025-10-08 18:09:37 -0400

    [ADD] La supression et l'achat d'un véhicule

--- resources/views/voitures/show.blade.php
+++ resources/views/voitures/show.blade.php
@@ -9,13 +9,30 @@
     <div class="container">
         <div class="show">
             <h2>voitures #{{$voiture->id}}</h2>
+            @if ($voiture->img)
+                <img src="../images/upload/{{$voiture->img}}" > 
+            @endif
             <p>{{ $voiture->marque }}</p>
             <p>{{ $voiture->modele }}</p>
             <p>{{ $voiture->prix }} $</p>
-            <img src="{{ $voiture->img }}" alt="">
         </div>
+
         <a href="{{ url("voitures/". $voiture->id . "/edit") }}">Modifier</a>
-        
+
+            <form method="post" action="{{ url('voitures/'. $voiture->id) }}" >
+            @csrf
+            @method('DELETE')
+                <button type="submit">Supprimer</button>
+           </form>
+
+
+            <form action="{{ route('achats.store') }}" method="POST" enctype="multipart/form-data">
+            @csrf
+                <input type="hidden" name="id_utilisateur" value="1" >
+                <input type="hidden" name="id_voiture" value="{{ $voiture->id }}">
+                {{ $voiture->dispo == 0 }}
+            <button type="submit">Acheter</button>
+            </form>
     </div>
 
 
