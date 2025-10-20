75cc4adc14a6c7453888d90d44341118276438eb
SamyMZA <2393410@cmontmorency.qc.ca>
2025-10-20 16:34:40 +0200

    [ADD] Modifier une image dans le view

--- resources/views/voitures/edit.blade.php
+++ resources/views/voitures/edit.blade.php
@@ -11,12 +11,13 @@
     <div class="row">
         <h2>Modifier une voiture</h2>
 
-        <form method="post" action="{{ url('voitures/'. $voiture->id) }}" >
+        <form method="post" action="{{ url('voitures/'. $voiture->id) }}" enctype="multipart/form-data">
             @method('PATCH')
             @csrf
             <p>Marque :</p> <input type="text" id="marque" name="marque" value="{{ $voiture->marque }}">
             <p>Modele :</p> <input type="text" id="modele" name="modele" value="{{ $voiture->modele }}">
             <p>Prix :</p> <input type="text" id="prix" name="prix" value="{{ $voiture->prix }}">
+            <p>Image:</p> <input type = "file" name= "img"  id = "img"  accept="image/*">
             <button type="submit">Modifier</button>
         </form>
         <a href="{{ url('voitures/'.$voiture->id) }}">retour</a>
